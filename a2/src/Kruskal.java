7aa62945d937f85889a7605e8d3e998468f0e981
Colton Campbell <campbell.colton3@gmail.com>
2020-11-11 20:00:00 -0500

    implement IsSafe() and kruskal() for minimum spanning trees

--- a2/src/Kruskal.java
+++ a2/src/Kruskal.java
@@ -2,18 +2,30 @@ import java.util.*;
 
 public class Kruskal{
 
+    /* take a graph object of the class WGraph as an input
+    * @return another WGraph object which will be the MST of the input graph
+    */
     public static WGraph kruskal(WGraph g){
-
-        /* Fill this method (The statement return null is here only to compile) */
-        
-        return null;
+        WGraph mst = new WGraph();
+        DisjointSets sets = new DisjointSets(g.getNbNodes());
+        // iterate through edges in monotonic increasing order of weight so we get visit light edges first
+        for (Edge e : g.listOfEdgesSorted()) {
+            if (IsSafe(sets, e)) {
+                // add edge to the growing MST
+                mst.addEdge(e);
+                // union the sets containing the edge's nodes
+                sets.union(e.nodes[0], e.nodes[1]);
+            }
+        }
+        return mst;
     }
 
+    /* considers a partition of the nodes p and an edge e
+    * @return True if it is safe to add the edge e to the MST, and False otherwise
+    */
     public static Boolean IsSafe(DisjointSets p, Edge e){
-
-        /* Fill this method (The statement return 0 is here only to compile) */
-        return true;
-    
+        /* e is not safe to add if it creates a cycle; if e's nodes are in the same set, adding e will create a cycle */
+        return p.find(e.nodes[0]) == p.find(e.nodes[1]) ? false : true;
     }
 
     public static void main(String[] args){
